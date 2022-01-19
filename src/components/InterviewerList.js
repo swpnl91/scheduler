42fb46f1505bc6acd222054330100c0922ad487d
Swapnil Parab <swapnilparab@Swapnils-MacBook-Air.local>
2022-01-19 07:36:02 -0500

    build InterviewerList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+
+export default function InterviewerList(props) {
+  const { interviewers, value, onChange } = props;
+  
+  const parsedInterviewers = interviewers.map((elm) => {
+    if (value === elm.id) {
+      return <InterviewerListItem selected={elm.id === value} setInterviewer={() => onChange(elm.id)} id={elm.id} name={elm.name} avatar={elm.avatar} key={elm.id} />
+    }
+    return <InterviewerListItem setInterviewer={() => onChange(elm.id)} id={elm.id} name={elm.name} avatar={elm.avatar} key={elm.id} />
+  });
+
+  
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>
+  );
+}
+
