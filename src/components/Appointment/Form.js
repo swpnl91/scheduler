56d88e780ed4a43af2c6f2d95abc0d4cf154ef3e
Swapnil Parab <swapnilparab@Swapnils-MacBook-Air.local>
2022-01-19 17:16:29 -0500

    add props/state to appointment form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,34 +4,58 @@ import InterviewerList from 'components/InterviewerList';
 
 export default function Form(props) {
   const { currentStudent, currentInterviewer, interviewers, save, onCancel } = props;
- 
+  const [student, setStudent] = useState(currentStudent|| "" );
+  const [interviewer, setInterviewer ] = useState(currentInterviewer || null);
+  
+  
+
+  
+  const reset = function() {
+    setStudent((prev) => {
+      return prev = "";
+    })
+    
+    setInterviewer((prev) => {
+      return prev = null;
+    })
+  }
+
+  
+  const cancel = function() {
+    reset();
+    onCancel();
+    
+  }
+
+  
+  
 
   
   return (
     <main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off">
-      <input
-        className="appointment__create-input text--semi-bold"
-        name="name"
-        type="text"
-        placeholder="Enter Student Name"
-        /*
-          This must be a controlled component
-          your code goes here
-        */
-      />
-    </form>
-    <InterviewerList 
-      /* your code goes here */
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger /* your code goes here */>Cancel</Button>
-      <Button confirm /* your code goes here */>Save</Button>
-    </section>
-  </section>
-</main>
+      <section className="appointment__card-left">
+        <form onSubmit={(event) => {event.preventDefault()}} autoComplete="off">
+          <input onChange={(event) => {setStudent(event.target.value)}} 
+            value={student}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            data-testid="student-name-input"
+          />
+        </form>
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList 
+          interviewers={interviewers} onChange={setInterviewer} value={interviewer} currentInterviewer={currentInterviewer}
+        />
+      <section className="appointment__validation">{errorInterviewer}</section>
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => {validate()}}>Save</Button>
+        </section>
+      </section>
+    </main>
   )
 }
